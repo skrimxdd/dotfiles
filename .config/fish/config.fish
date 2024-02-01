a6c811d80bc584fa0d8ba0770192ea22bd15d45e
Dustin <136272013+skrim-github@users.noreply.github.com>
2024-02-01 03:04:49 +0100

    upload

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+	afetch
+    # Commands to run in interactive sessions can go here
+end
+
+set -g fish_greeting ""
+
+function fish_prompt
+    set -l cwd (prompt_pwd)
+    set -l home (string escape $HOME)
+
+    set_color -o white
+    echo -n "["
+    set_color -o cyan
+    echo -n "$USER"
+    set_color -o white
+    echo -n "]─["
+    set_color -o cyan
+    echo -n "$cwd"
+    set_color -o white
+    echo -n "]─❯ "
+end
