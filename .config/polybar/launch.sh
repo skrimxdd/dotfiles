9c34c3ccded090d3e802919a38da898a0db96199
Dustin <skrimmail@proton.me>
2024-04-21 13:27:50 +0200

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,4 +10,5 @@ polybar-msg cmd quit
 echo "---" | tee -a /tmp/polybar1.log
 polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar bar2 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bar3 2>&1 | tee -a /tmp/polybar1.log & disown
 echo "Bars launched..."
