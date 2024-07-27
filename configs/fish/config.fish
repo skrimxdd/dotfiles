b55f3d916345a46216a31f43f4898ac8db709b2b
kyra <skrimmail@proton.me>
2024-07-27 05:37:12 +0200

    updated configs

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -3,23 +3,7 @@ fish_config theme choose "Dracula Official"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-	pfetch
+    pfetch
 end
 
 set -g fish_greeting ""
-
-function fish_prompt
-    set -l cwd (prompt_pwd)
-    set -l home (string escape $HOME)
-
-    set_color -o white
-    echo -n "["
-    set_color -o cyan
-    echo -n "$USER"
-    set_color -o white
-    echo -n "]─["
-    set_color -o cyan
-    echo -n "$cwd"
-    set_color -o white
-    echo -n "]─❯ " 
-end
